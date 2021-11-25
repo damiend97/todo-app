fc306065f38ee943944f14441e8bae5efadecc70
damiend97 <durandamien1997@gmail.com>
2021-11-25 11:46:22 -0700

    changed awsmobile to awsconfig installed @aws-amplify/datastore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import awsconfig from './aws-exports'
 Amplify.configure(awsconfig);
 
 import { Auth } from 'aws-amplify';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import { DataStore } from '@aws-amplify/datastore';
 import { Todo } from './models';
 
